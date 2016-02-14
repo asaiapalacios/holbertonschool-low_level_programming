acda9c40d1fc17dec2312642491da92a9f302f6c
Asaia Palacios <asaia.palacios@gmail.com>
2016-02-13 22:08:53 -0800

    function that prints single-digit numbers

---
+++ dreams_and_dedication_are_a_powerful_combination/1-print_combination_1.c
@@ -0,0 +1,15 @@
+#include "my_functions.h"
+
+void print_combination_1(void){
+  int n;
+  for(n=0; n<10; n++) /* int n must print integers 0-9 */
+    {
+      if (n==9)
+	{print_number(n);
+	  return; /* statement terminates execution of function */
+	}
+      print_number(n);
+      print_char(',');
+      print_char(' ');
+    }
+}
