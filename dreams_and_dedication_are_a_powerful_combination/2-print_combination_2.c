71492b7926ad2bc6f9b50c8e34d270019d7b12a6
Asaia Palacios <asaia.palacios@gmail.com>
2016-02-14 23:16:44 -0800

    edits to 2-print_combination_2.c

---
+++ dreams_and_dedication_are_a_powerful_combination/2-print_combination_2.c
@@ -0,0 +1,14 @@
+#include "my_functions.h"
+
+void print_combination_2(void)
+
+  for (n=01; n<90; n+01){
+  \* work in progress *\
+
+    if (n==89)
+    print_char(n);
+    return;
+
+    print_number(n);
+    print_char(',');
+    print_char(' ');
