579a46e201dbe032a87f2183e9717affd62572c2
Asaia Palacios <asaia.palacios@gmail.com>
2016-02-19 09:29:37 -0800

    Update 1-print_combination_1.c

--- dreams_and_dedication_are_a_powerful_combination/1-print_combination_1.c
+++ dreams_and_dedication_are_a_powerful_combination/1-print_combination_1.c
@@ -5,10 +5,11 @@ void print_combination_1(void){
   for ( n=0; n<10; n++ ) { /* int n must print integers 0-9 */
       if (n==9)
 	{print_number(n);
-	 return; /* statement terminates execution of function */
 	}
-      print_number(n);
-      print_char(',');
-      print_char(' ');
+      else {
+      	print_number(n);
+      	print_char(',');
+      	print_char(' ');
+	}
     }
 }
