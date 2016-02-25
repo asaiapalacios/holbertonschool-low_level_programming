8df04caecf879f813cfcf9381418e74e33d70462
Asaia Palacios <asaia.palacios@gmail.com>
2016-02-24 20:04:41 -0800

    Update 0-first_digit.c
    Edits: function should be maximum 25 lines long

--- its_tuesday_dont_forget_to_be_awesome/0-first_digit.c
+++ its_tuesday_dont_forget_to_be_awesome/0-first_digit.c
@@ -1,28 +1,22 @@
 /* Write a function that returns the first digit of a number */
-
 int first_digit(int n) /* function first_digit takes 1 argument (int n) & returns an (int) */
 {
   if ( n == 0 )
     return(n);
   else if ( n > 0 && n < 10 )
     return(n);
-  else if ( n > 10 )
-    {
-      while ( n >= 10 )
-      {
+  else if ( n > 10 ) {
+      while ( n >= 10 ){
 	n = n / 10;
       }
       return(n);
     }
-  else if ( n < -1 && n > -9 )
-    {
+  else if ( n < -1 && n > -9 ) {
       n = n * -1;
       return(n);
     }
-  else 
-    {
-      while ( n <= -10 )
-	{
+  else {
+      while ( n <= -10 ){
 	  n = n / 10;
 	}
       n = n * -1;
