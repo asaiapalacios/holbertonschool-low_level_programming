9216172180e8b70be18df8212b4d2b4cc3f67e45
Asaia Palacios <asaia.palacios@gmail.com>
2016-02-28 10:52:29 -0800

    Include comments to the function

--- i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
+++ i_ran_into_isosceles_he_had_a_great_idea_for_a_new_triangle/0-triangles.c
@@ -1,3 +1,4 @@
+/* write a function that prints n triangles of height h */
 #include <stdio.h>
 void triangles_in_the_term(int h, int n){
 
@@ -6,12 +7,12 @@ int space;
 int star;
 int ncopy;
  
- for (ncopy=1; ncopy<=n; ncopy++){ /*   */
-  for(row=1; row<=h; row++){
-    for(space=h-row; space>0; space--){
+for (ncopy=1; ncopy<=n; ncopy++){ /* for loop for n triangles */
+  for(row=1; row<=h; row++){ 
+    for(space=h-row; space>0; space--){ /* for loop to determine spaces before star */
       printf(" ");
     } 
-    for(star=1; star<=row*2-1; star++){
+    for(star=1; star<=row*2-1; star++){ /* for loop to determine starts per row */
       printf("*");
     }
     printf("\n");
