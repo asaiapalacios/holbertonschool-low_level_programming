2dfe13851ada8697e7eb7540c626bcf67bafb8ed
Asaia Palacios <asaia.palacios@gmail.com>
2016-03-16 15:04:06 -0700

    Iterate initialization. Work in progress

--- chemistry_is_the_melodies_you_can_play_on_vibrating_strings/1-nconcat_strings.c
+++ chemistry_is_the_melodies_you_can_play_on_vibrating_strings/1-nconcat_strings.c
@@ -2,11 +2,14 @@ char *nconcat_strings(char *dest, const char *src, int n)
 /* Write a function that concatenates two strings. Prototype: char *nconcat_strings(char *dest, const char *src, int n); Your function should work exactly like strncat */
 
 {
-  size_t i;
+  int i=0;
+  int dest_len;
   
   /* This loop stores the length of dest in i. It counts the number of char in dest */
-  for(i=0; dest[i]!='\0'; i++);
-
+  while(dest[i]!='\0') 
+    {
+      i++;
+    }
   /* Append portion of src string at the end of dest string */
   for(i = 0; i < n && src[i] != '\0'; i++)
     {
