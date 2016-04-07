e76df8ab9ad60c1503b8005c57c475a24ea9e214
Asaia Palacios <asaia.palacios@gmail.com>
2016-04-06 23:18:52 -0700

    Function pointers. First commit

---
+++ mistakes_are_always_forgivable_if_one_has_the_courage_to_admit_them/0-array_iterator.c
@@ -0,0 +1,12 @@
+#include "my_functions.h"
+
+/* Function prototype */
+void array_iterator(int *array, int size, void (*action_func)(int));
+
+/* Function implementation */
+void array_iterator(void (*action_func)(int elem))
+{
+  *action_func(int elem);
+}
+
+
