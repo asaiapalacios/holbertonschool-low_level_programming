4d97e8c2414161c86bf95d0e255e786cecd8b893
Asaia Palacios <asaia.palacios@gmail.com>
2016-04-28 18:07:09 -0700

    Task 4 Complete - Yay

--- cisdoublefun_day_2_pointers/4-string_to_integer.c
+++ cisdoublefun_day_2_pointers/4-string_to_integer.c
@@ -2,41 +2,45 @@
 
 int string_to_integer(char *s)
 {
-  /* Note: string is an array of characters pointed to by ...*s? */
-  int i;
-  int num;
+  int i, sign, rest;
+  long num;
 
+  rest = 0;
+  sign = 0;
   num = 0;
-  /* Set up loop through the string */
   i = 0;
-  
-  while(s[i] != '\0')
+  while(s[i] !='\0')
     {
-  /* Exit loop as soon as you find the first number using break */
-    if(s[i] '0'   s[i]  '9')
-      {
-	i++;
-      }
-  /* Convert the string into decimal value */
-    else
-      {
-	num = num * 10 + (*(s+i) - '0');
-	i++;
-      }
+      if(s[i] >= '0' && s[i] <= '9')
+        {
+              num = num * 10 + (s[i] - '0');
+              rest = 1;
+              i++;
+        }
+      else if(s[i] == '-') /* Count how many negative signs */
+        {
+          sign++;
+          i++;
+        }
+      else /* Count the rest of char that are not num nor -+ signs */
+        {
+          if(rest == 1)
+            {
+              break;
+            }
+          else
+            {
+            i++;
+            }
+        }
     }
-  /* If number is bigger than max value of int or smaller than min value of int, return 0 */
+  if(sign % 2 == 1)
+    {
+      num = num * -1;
+    }
   if(num > 2147483647 || num < -2147483648)
     {
       num = 0;
     }
-  return ();
+  return((int)num);
 }
-
-/* PREVIOUS ATTEMPT BELOW TO PRINT INTEGERS */
-  /* ((*s >= '0') && (*s <= '9'))
-    {
-    }
-    return(*s);*/
-
-/* ALSO */
-  /* Set flags when digit values are seen or not seen (decide which approach), then break out of the loop */
