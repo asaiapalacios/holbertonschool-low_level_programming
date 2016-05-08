4f699acb28974e9146e45d9c491f47f4ee19e55e
Asaia Palacios <asaia.palacios@gmail.com>
2016-05-08 14:59:26 -0700

    Update to task 1

--- linked_lists/8-add_node2.c
+++ linked_lists/8-add_node2.c
@@ -4,13 +4,17 @@
 
 int stringlen(char *str);
 char *strcopy(char *dest, char *src);
+List *find_last_node(List **list);
+
 /* Function adds a node to the list */
 int add_node(List **list, char *content)
 {
   char *copy_of_str;
   int string_len;
-  /* Declaring new ptr to List; ptr will hold addr of our new node */
-  List *node;
+ 
+  List *node;   /* Declaring new ptr to List; ptr will hold addr of our new node */
+  List *last;
+
   /* Allocate enough space in memory to hold new node */
   node = malloc(sizeof(List));
   if(node == NULL) /* If malloc function fails, raise an error */
@@ -18,14 +22,33 @@ int add_node(List **list, char *content)
   /* Copy content given as a parameter into a newly allocated space in memory */
   string_len = stringlen(content);
   copy_of_str = malloc(sizeof(char) * string_len);
-  /* if(copy_of_str == NULL)                                                                     
-     return NULL; */
+  /* COMMENTED OUT
+     if(copy_of_str == NULL)                                         
+     return NULL; 
+   */
   copy_of_str = strcopy(copy_of_str, content);
-  /* Initialize all the data of the new node */
+  
+  /* Initialize all the data of the new node.
+     2nd node's str element holds the pointer to the newly allocated & 
+     stored string (&copy_of_str)
+  */
   node->str = copy_of_str;
-  /* Make the first node 'next' ptr point to the first element of node (by accessing its addr) */
-  (*list)->next = node;
-  /* Make ptr 'next' of 2nd node point to NULL, so the 2nd node will now be the last node of our list */
+  
+  /* Make the 1st node (list is pnting to 1st node) 'next' ptr point 
+     to the first element of 2nd node (by accessing its addr).
+     Need if statement to ensure 1st node points to addr of 2nd node 
+  */ 
+  if(*list == NULL)
+    {
+      *list = node;
+    }
+    /* ELSE, find last node and designate the new node to follow */
+    last = find_last_node(list);
+    last->next = node;
+    }
+  /* Make ptr 'next' of 2nd node point to NULL, 
+     so the 2nd node will now be the last node of our list
+  */
   node->next = NULL;
   return(0);
 }
@@ -34,7 +57,6 @@ int add_node(List **list, char *content)
 char *strcopy(char *dest, char *src)
 {
   int i;
-
   for(i = 0; src[i] != '\0'; i++)
     {
       dest[i] = src[i];
@@ -47,9 +69,22 @@ char *strcopy(char *dest, char *src)
 int stringlen(char *str)
 {
   int i;
-
-  for(i = 0; str[i] != '\0'; i++)
-    {
-    }
+  for(i = 0; str[i] != '\0'; i++) {}
   return (i);
+}
+
+/* find_last_node takes the pointer to the front of the list;
+continues to next pointer in each node in the list until reaching 
+a NULL pointer. 
+*/
+List *find_last_node(List **list)
+{
+  List *node;
+
+  node = *list; /* Node pointer points to the front/beginning of the list */
+  while(node->next != NULL) 
+    {
+      node = node->next; /* We want node->next to point to nothing (NULL) */
+    }
+  return node; /* Return pointer to the current last node pointer in the list */
 }
