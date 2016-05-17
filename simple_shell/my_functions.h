fac63937043e1baade9c5a9733e3a4b47c9f370d
Asaia Palacios <17@p4-2.hbtn.io>
2016-05-17 00:03:44 -0700

    Edits to my_functions.h

--- simple_shell/my_functions.h
+++ simple_shell/my_functions.h
@@ -12,4 +12,3 @@ void print_string(char *str);
 char *string_concat(char *dest, const char *src);
 int str_ncomp(char *s1, char *s2, int c);
 void set_return(int *ret_value, char *s);
-void set_return(int *ret_value, char *s);
