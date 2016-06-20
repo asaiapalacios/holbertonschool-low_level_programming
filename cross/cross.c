29daf735680e4fbabe7655490630df79188b98aa
Asaia Palacios <asaia.palacios@gmail.com>
2016-06-20 10:43:53 -0700

    Update cross.c

--- cross/cross.c
+++ cross/cross.c
@@ -3,13 +3,13 @@
 int main()
 {
   int i, j, n;
-
-  scanf("%d", &n);
+/*Change scanf and printf*/
+  scanf("%d", &n); 
 
   for(i=0; i<n; i++) {
     for(j=0; j<n; j++) {
       if(i==j) {
-        printf("\");
+        printf("\\");
       }
       if(j==(n-i-1)) {
         printf("/");
